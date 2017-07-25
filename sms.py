148eba3efc9c433d87da771067de098646325741
Jaya Surya Kommireddy <kjayasurya97@gmail.com>
2017-07-25 21:21:19 +0530

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,9 +1,9 @@
 from twilio.rest import TwilioRestClient
 
-client = TwilioRestClient("ACaf5326775ed7f280cb76882b3202c431", "2a2c55dfc508eaac902d45e4bbe89f68")
+client = TwilioRestClient(", "")
 
 
 
 def send_sms():
-    client.messages.create(from_="+13363106998",to="+919944820260",body="Intruder Alert,Please check your mail and click this link for live stream" 'https://www.ivideon.com/my/cameras/devices')
+    client.messages.create(from_="from_number",to="to number",body="Intruder Alert,Please check your mail and click this link for live stream" 'https://www.ivideon.com/my/cameras/devices')
     print("SMS Sent to Registered Mobile")
